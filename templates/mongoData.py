4bd07f5638a4b1a67adb432d8d698d53201e7486
Sankhojjal Chatterjee <M1039122@mindtree.com>
2018-08-19 18:52:16 +0530

    Database connected

--- templates/mongoData.py
+++ templates/mongoData.py
@@ -1,7 +1,33 @@
 import pymongo
-myclient = pymongo.MongoClient("mongodb://localhost:27017")
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
-dataBase=[
-    {"name":"Sankhojjal"}
-]
+def mongoPythonConnection():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["customers"]
+    dataBase=[
+        { "_id": 1,
+            "name": "Sankhojjal", 
+            "password": 1234,
+            "balance": 100000,
+            "addPayee": {
+                "name": "Podu",
+                "acc_det": 2000
+            }},
+            { "_id": 2,
+            "name": "Prateek", 
+            "password": 1234,
+            "balance": 100000,
+            "addPayee": {
+                "name": "Subham",
+                "acc_det": 2000
+            }},
+            { "_id": 3,
+            "name": "Jeeshan", 
+            "password": 1234,
+            "balance": 100000,
+            "addPayee": {
+                "name": "Hagi",
+                "acc_det": 2000
+            }}
+    ]
+    x = mycol.insert_many(dataBase)
+    print(x.inserted_ids)
